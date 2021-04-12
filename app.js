b17145694f0723d15fe5575f8a047049dd9d49dc
Nazmul Hasan Robin <robin.sust08@gmail.com>
2021-04-13 01:15:24 +0600

    changes done

--- app.js
+++ app.js
@@ -1,37 +1,50 @@
 (
     function () {
         'use strict';
-        angular.module('LunchCheck', [])
-            .controller('LunchCheckController', LunchCheckController);
+        angular.module('ShoppingListCheckOff', [])
+            .controller('ToBuyController', ToBuyController)
+            .controller('AlreadyBoughtController', AlreadyBoughtController)
+            .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-            LunchCheckController.$inject = ['$scope'];
+        ToBuyController.$inject = ['ShoppingListCheckOffService'];
+        AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
-        function LunchCheckController($scope, $filter){
-            $scope.lunchItems = '';
-            $scope.lunchMessage = '';
+        function ToBuyController(ShoppingListCheckOffService) {
+            var tobuy = this;
+            tobuy.items = ShoppingListCheckOffService.tobuy;
 
-            $scope.calculateLunchItems = function(){
-                let totalItemsArr = $scope.lunchItems.split(',');
-                var filtered = totalItemsArr.filter(function (el) {
-                    return el != '';
-                  });
-                  console.log(filtered);
-                let totalItems = 0;
+            tobuy.buyItem = function (itemIndex) {
+                ShoppingListCheckOffService.buyItem(itemIndex);
+            };
+        }
 
-                if($scope.lunchItems != ''){
-                    totalItems = filtered.length;
-                }
-                console.log(totalItems);
-                let message = '';
-                if(totalItems > 0 && totalItems <= 3){
-                    message = 'Enjoy!';
-                }else if (totalItems > 3){
-                    message = 'Too much!';
-                }else if (totalItems == 0){
-                    message = 'Please enter data first';
-                }
-                $scope.lunchMessage = message;
-            }
+        function AlreadyBoughtController(ShoppingListCheckOffService) {
+            var bought = this;
+            bought.items = ShoppingListCheckOffService.bought;
+        }
+
+        function ShoppingListCheckOffService() {
+            var service = this;
+            service.tobuy = [{ name: "Apple", quantity: 3 },
+            { name: "Orange", quantity: 5 },
+            { name: "Banana", quantity: 20 },
+            { name: "Lunch", quantity: 5 },
+            { name: "Bread", quantity: 11 }];
+
+            service.bought = [];
+
+            service.buyItem = function (itemIndex) {
+                service.bought.push(service.tobuy[itemIndex]);
+                service.tobuy.splice(itemIndex, 1);
+            };
+
+            service.getToBuy = function () {
+                return service.tobuy;
+            };
+
+            service.getBought = function () {
+                return service.bought;
+            };
         }
     }
 
